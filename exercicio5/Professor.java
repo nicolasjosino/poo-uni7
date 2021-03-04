0f7a9c70d675c377dd9e74f474f4fa6547842e7c
repl.it user <replituser@example.com>
2021-03-04 01:27:34 +0000

    heranca

---
+++ exercicio5/Professor.java
@@ -0,0 +1,17 @@
+
+public class Professor extends Funcionario {
+
+  private String[] disciplinas;
+
+  public Professor() {}
+
+  public void setDisciplinas(String[] disciplinas) {
+    this.disciplinas = disciplinas;
+  }
+
+  public String[] getDisciplinas() {
+    return disciplinas;
+  }
+
+
+}
