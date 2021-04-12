c14a0d2f410e0ae8dc50fc0d039a07032ed8c0de
repl.it user <replituser@example.com>
2021-04-12 23:34:28 +0000

    aula de hoje

--- exercicio7/banco/produtos/ContaCorrente.java
+++ exercicio7/banco/produtos/ContaCorrente.java
@@ -8,6 +8,15 @@ public class ContaCorrente extends Produto {
     setSaldo(saldoAtual + rendimento);
   }
 
+  public void transferir(double valor, ContaCorrente contaDestino) throws Exception {
+    if (valor <= getSaldo()) {
+      this.setSaldo(this.getSaldo() - valor);
+      contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+    } else {
+      throw new Exception();
+    }
+  }
+
   public String getTipo() {
     return "Conta Corrente";
   }
@@ -15,5 +24,21 @@ public class ContaCorrente extends Produto {
   static int soma(int a, int b) {
     return a + b;
   }
+
+  public static void main(String[] args) {
+    ContaCorrente cc1 = new ContaCorrente();
+    ContaCorrente cc2 = new ContaCorrente();
+
+    try {
+      cc1.transferir(10.0, cc2);
+      cc2.pagarConta(5, cc3);
+      cc2.pagarConta(50, cc3);
+    } catch (Exception e) {
+      throw new Exception();
+    }
+  }
+
+  // main -> B -> C -> D -> E -> F
+  
 
 }
